600f3b7a4619abc5649220df45e56559e9a9c167
TobiasCarlsson <tobias.carlzzon@gmail.com>
2021-11-07 17:44:04 +0100

    Adding gitignore

--- src/main.ino
+++ src/main.ino
@@ -19,7 +19,7 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+int test = 4;
 int right = 2;
 int left = 3;
 int confirm = 4; 
